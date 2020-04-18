9b500ae6dd19a4d06cd50ce291150acb3033dd9d
David Kucher <mrdkucher@gmail.com>
2020-04-18 12:34:53 -0500

    minor cleanup

--- XNOR-Net-PyTorch/CIFAR_10/multiBlobDetector.py
+++ XNOR-Net-PyTorch/CIFAR_10/multiBlobDetector.py
@@ -7,11 +7,6 @@ img = cv2.imread('data/cheetah_results/cheetah_frm00066.png', cv2.IMREAD_GRAYSCA
 cv2.imshow('frame', img)
 cv2.waitKey(0)
 
-# # set a thresh
-# thresh = 100
-# # get threshold image
-# ret, thresh_img = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
-
 # find contours
 contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -29,25 +24,3 @@ img_contours = orig.copy()
 
 cv2.imshow('frame', img_contours)
 cv2.waitKey(0)
-
-
-# # Standard imports
-# import cv2
-# import numpy as np;
-
-# # Read image
-# im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
-
-# # Set up the detector with default parameters.
-# detector = cv2.SimpleBlobDetector()
-
-# # Detect blobs.
-# keypoints = detector.detect(im)
-
-# # Draw detected blobs as red circles.
-# # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-# im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-# # Show keypoints
-# cv2.imshow("Keypoints", im_with_keypoints)
-# cv2.waitKey(0)
